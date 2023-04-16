0983c8f91681f7ff86dee914a260747378f8ee0b
Artur Pilarek <artur.pilarek@gmail.com>
2023-04-16 12:51:14 +0200

    Finished creating crud api operations

--- src/pages/api/cities/[cityName].js
+++ src/pages/api/cities/[cityName].js
@@ -10,15 +10,46 @@ export default async (req, res) => {
       .collection("cities")
       .find({ name: cityName.toLowerCase() })
       .toArray()
+
     // Get city by name
     if (req.method === "GET") {
       res.json(city[0])
+
       // Modify city by name
     } else if (req.method === "PUT") {
+      const { name, country, population } = req.body
+
+      if (!name || !country || !population) {
+        res.status(422).json({ message: "Invalid input." })
+        return
+      }
+
+      if (city[0].name !== name) {
+        res.status(422).json({ message: "City name cannot be changed." })
+        return
+      }
+
+      const newCity = await db.collection("cities").updateOne(
+        { name: cityName.toLowerCase() },
+        {
+          $set: {
+            name,
+            country,
+            population,
+            dateAdded: new Date().toISOString(),
+          },
+        }
+      )
+
       res.status(201).json({ message: "Success!" })
     }
+
     // Delete city by name
     else if (req.method === "DELETE") {
+      const newCity = await db
+        .collection("cities")
+        .deleteOne({ name: cityName.toLowerCase() })
+
       res.status(201).json({ message: "Success!" })
     }
   } catch (e) {
